7d9d621b5b3879073271b6a7428af8b9dafda9f4
Vivek Chandra Sardar <chandravivek52@gmail.com>
2023-11-14 06:17:00 +0530

    easybank, version-05 - replace EasyBankUserDetails with EasyBankUsernamePasswordAuthenticationProvider ...

---
+++ EasyBank5/src/main/java/com/vcs/easybank/EasyBankApplication5.java
@@ -0,0 +1,13 @@
+package com.vcs.easybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBankApplication5 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyBankApplication5.class, args);
+	}
+
+}
